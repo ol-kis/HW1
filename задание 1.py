621580db2120cde7fed1d2f59f4ad95a3b8a9d5d
ol-kis <99480768+ol-kis@users.noreply.github.com>
2022-07-11 09:22:32 +0300

    Add files via upload

---
+++ задание 1.py
@@ -0,0 +1,14 @@
+def max_num(n,t):
+    return max(n,t)
+
+def max_num3(n,t,m):
+    return max(n,t,m)
+
+def even_num(n):
+    return n%2==0
+
+def even_num2(n):
+    return [i for i in range(1,n) if i%2==0]
+
+
+
